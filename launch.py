136c620cd67240731d6a89cc18cf163c12c8604b
desertocean <desertwreckage@outlook.com>
2024-02-24 20:35:19 +0800

    update

--- launch.py
+++ launch.py
@@ -128,13 +128,20 @@ def build_params(args, config='./arg_config.json'):
             elif 'default' in value:
                 setattr(args, key, value['default'])
         elif isinstance(value, dict) and 'need_change_url' in value:
-            local_url = download(getattr(args,key), args.task_dir, None,value['default_ext'])
-            setattr(args, key, local_url)
+            url_str=getattr(args, key)
+            if isinstance(url_str, str):
+                local_url = download(url_str, args.task_dir, value['default_file_name'] if 'default_file_name' in value else None, value['default_ext'])
+                setattr(args, key, local_url)
+            elif isinstance(url_str, list):
+                local_url_strs=[]
+                for index, url_ in enumerate(url_str):
+                    local_url_strs.append(download(url_, args.task_dir, f"{value['default_file_name']}_{index}",value['default_ext']))
+                setattr(args, key, local_url_strs)
             if 'sys_name' in value:
-                setattr(args, value['sys_name'], local_url)
+                setattr(args, value['sys_name'], getattr(args, key))
         elif isinstance(value, bool):
                 str_val = getattr(args, key)
-                if str_val == '1' or str_val.lower() == 'ture':
+                if str_val==True or str_val == '1' or str_val.lower() == 'ture':
                     setattr(args, key, True)
                 else:
                     setattr(args, key, False)
